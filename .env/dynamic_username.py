202fbb0f3445197502f53c6c2606114c076f1b42
Your Name <tasnimhasan5070@gmail.com>
2024-03-20 00:54:22 +0600

    delete folder

--- .env/dynamic_username.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask
-from markupsafe import escape
-app =Flask(__name__)
-@app.route("/user/<user_name>")
-def dynamic_username(user_name):
-    return f"<h1>'my name is {user_name}.'</h1>"
-app.run()
